83794e116827cbf6dc40deeca382938bd36c078d
gnampally <gyaneshwarrao.nampally@nielsen.com>
2022-02-05 22:35:58 -0400

    union find

--- leetcode/graphs/redundantconnection/main.go
+++ leetcode/graphs/redundantconnection/main.go
@@ -1,39 +1,71 @@
 // https://leetcode.com/problems/redundant-connection/ - 8:26 , submitted 8:50 ,
+// solution and possible approaches. https://leetcode.com/problems/redundant-connection/discuss/1295887/Easy-Solution-w-Explanation-or-All-Possible-Approaches-with-comments
 package main
 
-var nodeChildren map[int]map[int]bool
+import "fmt"
 
+//   1,2,3,4,5
+//p [1,1,1,1,5]
+//r [4,1,1,1,1]
+
+// 2 -> 1 <-3
+
+//navigate through the edges. [1,2] => p[1,2] -> p[1,1]
+// [1,3] => r[2,1] -> r[3,1]
+// [3,4] => p[1,4] r[3,1]
+// [2,4] => p[1,1]
+
+//algorithm
+// get the edge
+// find the parents
+// if the parents are not equal -> compare ranks -> highest rank will be the parent, and the other node will be child.
+// increase the rank of parent by 1. -> the child node's parent should be updated.
+// go to the next edge.
+// if the parent of 2 node in that edge is the same, then cycle is identified.
 func main() {
-
+	edge := findRedundantConnection([][]int{{1, 2}, {1, 3}, {2, 3}})
+	fmt.Println(edge)
 }
 
+var rank, parent []int
+
 func findRedundantConnection(edges [][]int) []int {
-	nodeChildren := make(map[int]map[int]bool)
-
-	fillNodes := func(p, c int) {
-		if children, ok := nodeChildren[p]; ok {
-			children[c] = true
-			nodeChildren[p] = children
-			return
-		}
-		children := map[int]bool{c: true}
-		nodeChildren[p] = children
-	}
-	// fill in node and its children.
-	for _, edge := range edges {
-		fillNodes(edge[0], edge[1])
-		fillNodes(edge[1], edge[0])
+	noOfNodes := len(edges) + 1
+	rank = make([]int, 0, noOfNodes)
+	parent = make([]int, 0, noOfNodes)
+	for i := 0; i < noOfNodes; i++ {
+		rank = append(rank, 1)
+		parent = append(parent, i)
 	}
 
-	// consider all the nodes which are having children greater than 1
-	//traverse from end of the edges and figure out the first combination.
-	for i := len(edges) - 1; i > 0; i-- {
-		edge := edges[i]
-		e0c := nodeChildren[edge[0]]
-		e1c := nodeChildren[edge[1]]
-		if len(e0c) > 1 && len(e1c) > 1 {
-			return edge
+	for i := 0; i < len(edges); i++ {
+		x, y := edges[i][0], edges[i][1]
+		if !Union(x, y) {
+			return edges[i]
 		}
 	}
 	return []int{}
+}
+
+func findParent(n int) int {
+	if parent[n] == n {
+		return n
+	}
+	return findParent(parent[n])
+}
+
+func Union(x, y int) bool {
+	a, b := findParent(x), findParent(y)
+	if a == b {
+		return false
+	}
+	if rank[x] >= rank[y] {
+		rank[x] = rank[x] + 1
+		parent[b] = a
+		return true
+	}
+
+	rank[y] = rank[y] + 1
+	parent[a] = b
+	return true
 }
